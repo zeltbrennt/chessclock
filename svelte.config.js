0591aecd34e2c9fdd8d241df9783fa2cdf0914a7
zeltbrennt <zeltbrennt@gmail.com>
2025-07-01 15:56:01 +0200

    Add GH-Pages

--- svelte.config.js
+++ svelte.config.js
@@ -11,10 +11,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-			base: 'https://zeltbrennt.github.io/chessclock'
-		}
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	},
+	paths: {
+		base: "/chessclock"
 	}
 };
 
